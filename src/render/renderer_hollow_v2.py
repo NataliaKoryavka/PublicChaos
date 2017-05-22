bca84621a7ffe857c1746da369f590e293577847
svboeing <boykodjs4@ya.ru>
2017-05-22 21:00:52 +0300

    exceptions/walls/kill process when goal reached
    also minor fixes and @staticmethod-s.

--- src/render/renderer_hollow_v2.py
+++ src/render/renderer_hollow_v2.py
@@ -10,7 +10,7 @@ from math import exp
 import threading
 import time
 
-class Singleton(type):
+class Singleton(type): #отлажено
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
@@ -31,11 +31,12 @@ class Actor(threading.Thread):
         self.step = step #это заполняет seeder
         self.do_step = do_step #это ивент
         self.solve = solve
+        self.walker = False
         self.setDaemon(True)
         self.start()
         
     def run(self):
-        while True:
+        while self.walker == True:
             self.solve.wait()
             if np.linalg.norm(self.position - self.crowd.goal) > 0.01: #достижение цели - выбрать константу
                 actorSolver = FieldSolver(self.position, self.crowd, self.param, self.step)
@@ -47,15 +48,25 @@ class Actor(threading.Thread):
                 field_x_step = self.fieldGoal[1] + self.fieldComfortObstacle[1] + self.fieldComfortCrowd[1]
                 field_y_step = self.fieldGoal[2] + self.fieldComfortObstacle[2] + self.fieldComfortCrowd[2]
                 self.direct = np.array([-(field_x_step - field)/self.step, -(field_y_step - field)/self.step]) #антиградиент по определению
-
+            else:
+                self.walker = False
+                break
             self.do_step.wait()
             if np.linalg.norm(self.position - self.crowd.goal) > 0.01: #достижение цели - выбрать константу
                 self.position += self.direct*self.fieldSpeed
+            else:
+                self.walker = False
+                break
+
+class Wall(): #np.array([0,0]) дважды
+    def __init__(self, pos1, pos2):
+        self.pos1 = pos1
+        self.pos2 = pos2
 
 class Crowd(metaclass=Singleton): #Singleton
-    def __init__(self, actors, obstacles, goal):
+    def __init__(self, actors, walls, goal):
         self.actors = actors
-        self.obstacles = obstacles #WTF IS OBSTACLES
+        self.walls = walls #WTF IS OBSTACLES
         #self.seeders = []
         self.goal = goal #np.array([0, 0])
     
@@ -90,19 +101,20 @@ class Crowd(metaclass=Singleton): #Singleton
 
 class DensitySolver():
     #def __init__(self):
-        
-    def solveDensity(self, pos, crowd):
+    @staticmethod
+    def solveDensity(pos, crowd):
         num = 0
         radius = 5 #выбрать константу
-        for other in crowd:
-            if np.linalg.norm(pos - other.position) < radius:
-                num += 1
+        for actor in crowd.actors:
+            if actor.walker == True:
+                if np.linalg.norm(pos - actor.position) < radius:
+                    num += 1
         return num
 
 class FieldFunction():
     #def __init__(self):
-    
-    def linear(self, x, s_min, s_max, x_min, x_max, c): #отлажено
+    @staticmethod
+    def linear(x, s_min, s_max, x_min, x_max, c): #отлажено
         if x >= x_max:
             return s_min
         if x <= x_min:
@@ -111,8 +123,8 @@ class FieldFunction():
             return s_max + ((x - x_min)*(s_min - s_max))/(x_max - x_min)
         else:
             raise badparam
-            
-    def power(self, x, s_min, s_max, x_min, x_max, c): #отлажено
+    @staticmethod        
+    def power(x, s_min, s_max, x_min, x_max, c): #отлажено
         if x >= x_max:
             return s_min
         if x <= x_min:
@@ -151,26 +163,37 @@ class FieldSolver(FieldFunction, DensitySolver):
         self.f_min = param[18]
         self.f_max = param[19]
         
-    def fieldGoal(self):
+    def fieldGoal(self): #should be linear maybe
         x_goal = self.linear(self, np.linalg.norm(self.pos - self.crowd.goal), self.s_gmin, self.s_gmax, self.x_gmin, self.x_gmax, self.c_g)
         x_goal_x_step = self.linear(self, np.linalg.norm(self.pos + np.array([self.step, 0]) - self.crowd.goal), self.s_gmin, self.s_gmax, self.x_gmin, self.x_gmax, self.c_g)
         x_goal_y_step = self.linear(self, np.linalg.norm(self.pos + np.array([0, self.step]) - self.crowd.goal), self.s_gmin, self.s_gmax, self.x_gmin, self.x_gmax, self.c_g)
     return [x_goal, x_goal_x_step, x_goal_y_step]
     #добавить области видимости после реализации цели и препятстий
-    def fieldComfortObstacle(self):
+    def fieldComfortObstacle(self): #CHECK PARAMS RIGRGlkDSTRG
         summ = 0
-        for k in xrange(10):#? - число точек на границе препятствия. Ожидается реализация
-            summ += self.linear(self, np.linalg.norm(self.pos - self.crowd.obstacles[k]), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
-            summ_x_step += self.linear(self, np.linalg.norm(self.pos + np.array([self.step, 0]) - self.crowd.obstacles[k]), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
-            summ_y_step += self.linear(self, np.linalg.norm(self.pos + np.array([0, self.step]) - self.crowd.obstacles[k]), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
+        summ_x_step = 0
+        summ_y_step = 0
+        for wall in self.crowd.walls:
+            vec1 = wall.pos1 - self.pos
+            vec2 = wall.pos2 - self.pos
+            summ += self.linear(self, np.arccos(np.dot(vec1, vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
+            vec1_x_step = vec1 - np.array([self.step, 0])
+            vec2_x_step = vec2 - np.array([self.step, 0])
+            summ_x_step += self.linear(self, np.arccos(np.dot(vec1_x_step, vec2_x_step)/(np.linalg.norm(vec1_x_step)*np.linalg.norm(vec2_x_step))), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
+            vec1_y_step = vec1 - np.array([0, self.step])
+            vec2_y_step = vec2 - np.array([0, self.step])
+            summ_y_step += self.linear(self, np.arccos(np.dot(vec1_y_step, vec2_y_step)/(np.linalg.norm(vec1_y_step)*np.linalg.norm(vec2_y_step))), self.s_wmin, self.s_wmax, self.x_wmin, self.x_wmax, self.c_w)
         return [summ, summ_x_step, summ_y_step]
     
     def fieldComfortCrowd(self):
         summ = 0
-        for k in len(self.crowd.actors):# - число людей в толпе
-            summ += self.power(self, np.linalg.norm(self.pos - self.crowd.actors[k].position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
-            summ_x_step += self.power(self, np.linalg.norm(self.pos + np.array([self.step, 0]) - self.crowd.actors[k].position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
-            summ_y_step += self.power(self, np.linalg.norm(self.pos + np.array([0, self.step]) - self.crowd.actors[k].position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
+        summ_x_step = 0
+        summ_y_step = 0
+        for actor in self.crowd.actors:# - число людей в толпе
+                if actor.walker == True:
+                    summ += self.power(self, np.linalg.norm(self.pos - actor.position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
+                    summ_x_step += self.power(self, np.linalg.norm(self.pos + np.array([self.step, 0]) - actor.position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
+                    summ_y_step += self.power(self, np.linalg.norm(self.pos + np.array([0, self.step]) - actor.position), self.s_pmin, self.s_pmax, self.x_pmin, self.x_pmax, self.c_p)
         return [summ, summ_x_step, summ_y_step]
 
     def fieldSpeed(self):
@@ -192,8 +215,11 @@ class CrowdController(metaclass=Singleton): #singleton
                 self.do_step.set()
                 time.sleep(5)
                 self.do_step.clear()
-            except:
-                print("end")
+            except KeyboardInterrupt: 
+                self.solve.clear()
+                self.do_Step.clear()
+                break
+                
 
 class Constructor(metaclass=Singleton): #singleton
     def __init__(self, crowd_size, step, param): #ЭТО ВВОДИТСЯ ПОЛЬЗОВАТЕЛЕМ В ИНТЕРФЕЙСЕ
@@ -205,13 +231,17 @@ class Constructor(metaclass=Singleton): #singleton
         self.solve = threading.Event()
         self.crowdController
         self.seed()
-    def seed():
+    def seed(self):
         actors = []
         for i in range(self.crowd_size):
-            res = ???
-            new_actor = Actor(res, self.param, self.step, self.do_step, self.solve)
+            pos = ???
+            new_actor = Actor(pos, self.param, self.step, self.do_step, self.solve)
             actors.append(new_actor)
         self.crowd = Crowd(??? self.actors)
         for i in range(self.crowd_size):
             Actor.crowd = self.crowd
+            Actor.walker = True
         self.crowdController = CrowdController(self.do_step, self.solve)
+        
+
+if __name__ == '__main__':
