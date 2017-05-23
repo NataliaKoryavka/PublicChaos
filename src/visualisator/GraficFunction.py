219e646d513b442596d55118ffa840bde2da87ec
svboeing <boykodjs4@ya.ru>
2017-05-23 12:16:02 +0300

    np.array([x, y]) coords added

--- src/visualisator/GraficFunction.py
+++ src/visualisator/GraficFunction.py
@@ -6,28 +6,31 @@ Created on Tue May 23 02:11:37 2017
 """
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPolygon
+import numpy as np
 
 class Point:
-    def __init__(self, abs, ord):
-        self.abs = abs
-        self.ord = ord
+    def __init__(self, pos):
+        #self.abs = abs
+        #self.ord = ord
+        self.pos = pos #np.array([0,0])
 
     def x(self):
-        return(self.abs)
+        return self.pos[0]
 
     def y(self):
-        return(self.ord)
+        return self.pos[1]
 
     def myQPoint(self):
-        return QPoint(self.abs, self.ord)
+        return QPoint(self.pos[0], self.pos[1])
 
     @staticmethod
     def dist(point1, point2):
-        return ((point2.x() - point1.x()) ** 2 + (point2.y() - point1.y()) ** 2) ** 0.5
+        #return ((point2.x() - point1.x()) ** 2 + (point2.y() - point1.y()) ** 2) ** 0.5
+        return np.linalg.norm(point2.pos - point1.pos)
 
 class Node(Point):
-    def __init__(self,abs,ord,name):
-        super().__init__(abs,ord)
+    def __init__(self, pos, name):
+        super().__init__(pos)
         self.name = name
         self.isBeginning = False
         self.isEnd = False
@@ -49,29 +52,31 @@ class Node(Point):
         return self.name
     
     def extract(self):
-        return (self.x(), self.y())
+        return self.pos
     
     @staticmethod
-    def newNode(x,y,name):
-        v = Node(x,y,name)
+    def newNode(pos, name):
+        v = Node(pos, name)
         return v
     
 class Wall:
     def __init__(self,first,name):
         self.name = name
-        self.fn = first
+        self.fn = first #Point
+        self.pos1 = first.pos  #np.array([0,0])
         self.completed = False
         
     def endOfWall(self,end):
         if self.completed == False:
-            self.en = end
-            self.completed = True
-        self.pos = [fn,en]
+            self.en = end #Point
+            self.pos2 = end.pos #np.array([0,0])
+        #   self.completed = True
+        #self.pos = [fn,en]
         
     def extract(self):
         if self.completed:
-            pos = []
-            for n in self.pos:
-                pos.append((n.x(),n.y()))
-            return pos
+            #pos = []
+            #for n in self.pos:
+            #    pos.append((n.x(),n.y()))
+            return self.pos1, self.pos2 #возвращаем два вектора
         
